b8ef13c485f10da139ff60fe03643f4c96bc54cf
aleem ahmed <w3st.io2021@gmail.com>
2022-12-25 21:58:43 -0500

    init files

---
+++ contracts/CardinalProtocolGovernance.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+
+/* [import] */
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+/* [import-internal] */
+import "./interface/ICardinalProtocolGovernance.sol";
+
+
+/**
+* @title CardinalProtocolGovernance
+* @author harpoonjs.eth
+*/
+contract CardinalProtocolGovernance is
+	AccessControlEnumerable,
+	ICardinalProtocolGovernance
+{
+	/* [state-variables] */
+	/// @inheritdoc ICardinalProtocolGovernance
+	bytes32 public constant S = keccak256("S");
+	/// @inheritdoc ICardinalProtocolGovernance
+	bytes32 public constant A = keccak256("A");
+	/// @inheritdoc ICardinalProtocolGovernance
+	bytes32 public constant B = keccak256("B");
+	/// @inheritdoc ICardinalProtocolGovernance
+	bytes32 public constant C = keccak256("C");
+
+
+	/* [constructor] */
+	constructor ()
+	{
+		_setRoleAdmin(DEFAULT_ADMIN_ROLE, S);
+
+		_setupRole(S, _msgSender());
+	}
+}
+
+/**
+*    _________    ____  ____  _____   _____    __       ____  ____  ____  __________  __________  __ 
+*   / ____/   |  / __ \/ __ \/  _/ | / /   |  / /      / __ \/ __ \/ __ \/_  __/ __ \/ ____/ __ \/ / 
+*  / /   / /| | / /_/ / / / // //  |/ / /| | / /      / /_/ / /_/ / / / / / / / / / / /   / / / / /  
+* / /___/ ___ |/ _, _/ /_/ // // /|  / ___ |/ /___   / ____/ _, _/ /_/ / / / / /_/ / /___/ /_/ / /___
+* \____/_/  |_/_/ |_/_____/___/_/ |_/_/  |_/_____/  /_/   /_/ |_|\____/ /_/  \____/\____/\____/_____/
+*/
---
+++ contracts/interface/ICardinalProtocolGovernance.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+
+/**
+* @title ICardinalProtocolGovernance
+*/
+interface ICardinalProtocolGovernance 
+{
+	/**
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
+	* @return {bytes32} keccak256 value
+	*/
+	function S() external view returns (bytes32);
+
+	/**
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
+	* @return {bytes32} keccak256 value
+	*/
+	function A() external view returns (bytes32);
+
+	/**
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
+	* @return {bytes32} keccak256 value
+	*/
+	function B() external view returns (bytes32);
+
+	/**
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
+	* @return {bytes32} keccak256 value
+	*/
+	function C() external view returns (bytes32);
+}
